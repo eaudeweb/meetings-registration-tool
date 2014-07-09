a460187c33b591ff7c2b426f67c91a4f0a785b1e
Taygun <taygun.agiali@eaudeweb.ro>
2014-07-09 11:58:32 +0300

    Added meeting model

--- meetings/models.py
+++ meetings/models.py
@@ -14,3 +14,26 @@ class User(db.Model):
 
     def __repr__(self):
         return '{} {}'.format(self.first_name, self.last_name)
+
+
+class Meeting(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(35), nullable=False)
+    acronym = db.Column(db.String(10), nullable=False)
+    slug = db.Column(db.String(10))
+    type = db.Column(db.String(35), nullable=False)
+    date_start = db.Column(db.DateTime, nullable=False)
+    date_end = db.Column(db.DateTime, nullable=False)
+    venue_address = db.Column(db.String(70))
+    venue_city = db.Column(db.String(35), nullable=False)
+    venue_country = db.Column(db.String(35), nullable=False)
+    admin_name = db.Column(db.String(35))
+    admin_email = db.Column(db.String(35))
+    media_admin_name = db.Column(db.String(35))
+    media_admin_email = db.Column(db.String(35))
+    online_registration = db.Column(db.Boolean, nullable=False, default=True)
+    require_photo = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return '{}'.format(self.title)
