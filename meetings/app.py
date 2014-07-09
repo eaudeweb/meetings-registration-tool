f3f44a4ed28278a6f8d77c528392798b039edc42
Dragos Catarahia <dragos@catarahia.me>
2014-07-09 17:55:22 +0300

    Webassets and bootstrap 3 + jquery

--- meetings/app.py
+++ meetings/app.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from meetings.models import db
+from meetings.assets import assets_env
 
 
 def create_app(config={}):
     app = Flask(__name__, instance_relative_config=True)
     app.config.update(config)
     app.config.from_pyfile('settings.py')
+
+    assets_env.init_app(app)
     db.init_app(app)
 
     @app.route('/')
     def temp():
-        pass
-
+        from flask import render_template
+        return render_template('_layout.html')
     return app
---
+++ meetings/assets.py
@@ -0,0 +1,22 @@
+from flask.ext.assets import Environment, Bundle
+
+
+_BUNDLE_CSS = (
+    'css/bootstrap.min.css',
+    'css/bootstrap-theme.min.css',
+)
+
+
+_BUNDLE_JS = (
+    'js/lib/jquery.min.js',
+    'js/lib/bootstrap.min.js',
+)
+
+
+css = Bundle(*_BUNDLE_CSS, filters='cssmin', output='gen/static.css')
+js = Bundle(*_BUNDLE_JS, filters='jsmin', output='gen/static.js')
+
+
+assets_env = Environment()
+assets_env.register('css', css)
+assets_env.register('js', js)
