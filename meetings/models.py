43b49db1de16937315b5513ca5eddbcbaec44bd2
Taygun <taygun.agiali@eaudeweb.ro>
2014-07-09 15:02:03 +0300

    Changed the string fields models length

--- meetings/models.py
+++ meetings/models.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(35), nullable=False)
-    last_name = db.Column(db.String(35), nullable=False)
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(128))
 
@@ -19,23 +19,22 @@ class User(db.Model):
 class Meeting(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(35), nullable=False)
-    acronym = db.Column(db.String(10), nullable=False)
-    type_id = db.Column(db.String(35), db.ForeignKey('type.slug'),
+    title = db.Column(db.String(32), nullable=False)
+    acronym = db.Column(db.String(16), nullable=False)
+    type_id = db.Column(db.String(32), db.ForeignKey('type.slug'),
                         nullable=False)
     type = db.relationship('MeetingType',
                            backref=db.backref('meetings', lazy='dynamic'))
     date_start = db.Column(db.DateTime, nullable=False)
     date_end = db.Column(db.DateTime, nullable=False)
-    venue_address = db.Column(db.String(70))
-    venue_city = db.Column(db.String(35), nullable=False)
-    venue_country = db.Column(db.String(35), nullable=False)
-    admin_name = db.Column(db.String(35))
-    admin_email = db.Column(db.String(35))
-    media_admin_name = db.Column(db.String(35))
-    media_admin_email = db.Column(db.String(35))
+    venue_address = db.Column(db.String(128))
+    venue_city = db.Column(db.String(32), nullable=False)
+    venue_country = db.Column(db.String(32), nullable=False)
+    admin_name = db.Column(db.String(32))
+    admin_email = db.Column(db.String(32))
+    media_admin_name = db.Column(db.String(32))
+    media_admin_email = db.Column(db.String(32))
     online_registration = db.Column(db.Boolean, nullable=False, default=True)
-    require_photo = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return '{}'.format(self.title)
@@ -43,5 +42,5 @@ class Meeting(db.Model):
 
 class MeetingType(db.Model):
 
-    slug = db.Column(db.String(10), primary_key=True)
-    name = db.Column(db.String(35), nullable=False)
+    slug = db.Column(db.String(16), primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
