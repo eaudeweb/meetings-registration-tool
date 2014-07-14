876aedcfc75233f72e5c9cde11520beeedd8b638
Taygun <taygun.agiali@eaudeweb.ro>
2014-07-14 12:19:32 +0300

    Added two relevant test

--- testsuite/conftest.py
+++ testsuite/conftest.py
@@ -24,3 +24,22 @@ def app(request):
         app_context.pop()
 
     return app
+
+
+@fixture
+def session(request):
+    """Creates a new database session for a test."""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    session = db.create_scoped_session()
+
+    db.session = session
+
+    @request.addfinalizer
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        session.remove()
+
+    return session
--- testsuite/test_meeting.py
+++ testsuite/test_meeting.py
@@ -1,4 +1,18 @@
+from meetings.models import User
 
 
-def test(app):
-    assert True
+def test_model(app, session):
+    user = User(email='johndoe@eaudeweb.ro')
+
+    session.add(user)
+    session.commit()
+
+    count = User.query.count()
+    assert count == 1
+
+
+def test_view(app):
+    with app.test_client() as client:
+        response = client.get('/')
+
+    assert response.status_code == 200
