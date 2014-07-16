0b283a0a72a21eb432e7559a4238a39c0c27256d
Taygun <taygun.agiali@eaudeweb.ro>
2014-07-16 15:49:30 +0300

    Added edit view for staff

--- mrt/admin.py
+++ mrt/admin.py
@@ -2,7 +2,7 @@ from flask import render_template, Blueprint, request, redirect, url_for
 from flask.ext.login import login_required
 
 from mrt.models import db, Staff
-from mrt.forms.admin import StaffForm
+from mrt.forms.admin import StaffForm, StaffEditForm
 
 admin = Blueprint("adin", __name__)
 
@@ -32,4 +32,31 @@ def staff_add():
         db.session.commit()
         return redirect(url_for('.staff'))
 
-    return render_template('admin/add.html', form=form)
+    submit_url = url_for('.staff_add')
+
+    return render_template('admin/add.html',
+                           form=form,
+                           submit_url=submit_url)
+
+
+@admin.route('/admin/staff/<int:staff_id>/edit', methods=['GET', 'POST'])
+@login_required
+def staff_edit(staff_id=None):
+    staff = Staff.query.get_or_404(staff_id)
+
+    if request.method == 'GET':
+        form = StaffEditForm(obj=staff)
+    else:
+        form = StaffEditForm(request.form)
+
+        if form.validate():
+            form.populate_obj(staff)
+            db.session.add(staff)
+            db.session.commit()
+            return redirect(url_for('.staff'))
+
+    submit_url = url_for('.staff_edit', staff_id=staff_id)
+    return render_template('admin/add.html',
+                           form=form,
+                           staff_id=staff_id,
+                           submit_url=submit_url)
