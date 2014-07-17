3b6001cb84e2318881454e420dc84e0ba680a124
Dragos Catarahia <dragos@catarahia.me>
2014-07-17 16:08:25 +0300

    fix spaces in auth/views.py

--- mrt/auth/views.py
+++ mrt/auth/views.py
@@ -13,18 +13,15 @@ class Login(MethodView):
     def get(self):
         form = LoginForm()
         next = request.values.get('next')
-
         return render_template('auth/login.html', form=form, next=next)
 
     def post(self):
         form = LoginForm(request.form)
         next = request.values.get('next')
-
         if form.validate():
             user = form.get_user()
             login_user(user)
             return redirect(next or url_for('temp'))
-
         return render_template('auth/login.html', form=form, next=next)
 
 
@@ -39,14 +36,11 @@ class RecoverPassword(MethodView):
 
     def get(self):
         form = RecoverForm()
-
         return render_template('auth/recover.html', form=form)
 
     def post(self):
         form = RecoverForm(request.form)
-
         if form.validate():
             form.save()
             #TODO create token and timpstamp on user, send email with url+token
-
         return render_template('auth/recover.html', form=form)
