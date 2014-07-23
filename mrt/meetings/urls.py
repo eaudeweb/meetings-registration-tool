b3302a49f9067fb6b47315a264624ab517ae0919
Dragos Catarahia <dragos@catarahia.me>
2014-07-23 17:56:14 +0300

    remove / from meetings url

--- mrt/meetings/urls.py
+++ mrt/meetings/urls.py
@@ -5,7 +5,7 @@ from mrt.meetings import Meetings, MeetingEdit
 meetings = Blueprint('meetings', __name__, url_prefix='/meetings')
 
 
-meetings.add_url_rule('/', view_func=Meetings.as_view('home'))
+meetings.add_url_rule('', view_func=Meetings.as_view('home'))
 
 meeting_edit_func = MeetingEdit.as_view('edit')
 meetings.add_url_rule('/add', view_func=meeting_edit_func)
