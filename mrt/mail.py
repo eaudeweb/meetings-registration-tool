746dc5497f80eb7cd946e1056f2ab5862cbbe4c0
Taygun <taygun.agiali@eaudeweb.ro>
2014-07-24 18:03:18 +0300

    Removed hostname from settings.example

--- mrt/mail.py
+++ mrt/mail.py
@@ -1,4 +1,4 @@
-from flask import current_app as app
+from flask import current_app as app, request
 from flask.ext.mail import Mail, Message
 
 
@@ -6,21 +6,24 @@ mail = Mail()
 
 
 def send_reset_mail(email, token):
-    url = app.config.get('HOSTNAME') + 'reset/' + token
+    url = request.url_root + 'reset/' + token
     subject = "Reset your password"
     body = "Your reset link is: " + url
     sender = app.config['DEFAULT_MAIL_SENDER']
+
     msg = Message(subject=subject, body=body, sender=sender,
                   recipients=[email, ])
     mail.send(msg)
 
 
 def send_activation_mail(email, token):
-    url = app.config.get('HOSTNAME') + 'reset/' + token
+    url = request.url_root + 'reset/' + token
     subject = "Activate your account"
     body = ("Your user has been created. To complete your activation "
             "follow the link: " + url)
     sender = app.config['DEFAULT_MAIL_SENDER']
+    body = "Your user has been created. To complete your activation \
+follow the link: " + url
 
     msg = Message(subject=subject, body=body, sender=sender,
                   recipients=[email, ])
