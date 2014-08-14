bf006fccb03ee49c75a43dc1a39f7ad7d20e1103
Taygun <taygun.agiali@eaudeweb.ro>
2014-08-14 16:07:09 +0300

    Filters for ActivityLog list view

--- mrt/meetings/log.py
+++ mrt/meetings/log.py
@@ -1,13 +1,13 @@
 from flask import g, render_template
-from flask import jsonify, flash, url_for
+from flask import jsonify, flash, url_for, request
 from flask.views import MethodView
 from flask.ext.login import current_user as user
 
 from blinker import ANY
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from mrt.models import db, Participant, MediaParticipant
-from mrt.models import ActivityLog, MailLog, Staff
+from mrt.models import ActivityLog, MailLog, Staff, RoleUser
 from mrt.signals import activity_signal
 
 
@@ -60,6 +60,23 @@ class MailLogDetail(MethodView):
 class ActivityLogs(MethodView):
 
     def get(self):
+        staffs = RoleUser.query.filter_by(meeting=g.meeting)
         activities = ActivityLog.query.filter_by(meeting=g.meeting)
+
+        staff_id = request.args.get('staff_id', 0)
+        seconds = request.args.get('time', 0)
+        name = request.args.get('name', 0)
+
+        if staff_id:
+            activities = activities.filter_by(staff_id=int(staff_id))
+
+        if seconds:
+            relative_date = datetime.now() - timedelta(seconds=int(seconds))
+            activities = activities.filter(ActivityLog.date > relative_date)
+
+        if name:
+            activities = activities.filter_by(participant_name=name)
+
         return render_template('meetings/log/activity.html',
-                               activities=activities)
+                               activities=activities,
+                               staffs=staffs)
