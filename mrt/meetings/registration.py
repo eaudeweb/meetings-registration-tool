602465a25a3162db66ee37b6d103481641c22490
Taygun <taygun.agiali@eaudeweb.ro>
2014-10-22 14:19:59 +0300

    [Registration] Added closed registration template

--- mrt/meetings/registration.py
+++ mrt/meetings/registration.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from flask import render_template, request
+from flask import g, render_template, request
 
 from mrt.forms.meetings import custom_form_factory
 from mrt.forms.meetings import RegistrationForm
@@ -8,17 +8,21 @@ from mrt.forms.meetings import RegistrationForm
 class Registration(MethodView):
 
     def get(self):
-        Form = custom_form_factory(registration_fields=True,
-                                   form=RegistrationForm)
-        form = Form()
-        return render_template('meetings/registration/form.html',
-                               form=form)
+        if g.meeting.online_registration:
+            Form = custom_form_factory(registration_fields=True,
+                                       form=RegistrationForm)
+            form = Form()
+            return render_template('meetings/registration/form.html',
+                                   form=form)
+        return render_template('meetings/registration/closed.html')
 
     def post(self):
-        Form = custom_form_factory(registration_fields=True,
-                                   form=RegistrationForm)
-        form = Form(request.form)
-        if form.validate():
-            form.save()
-        return render_template('meetings/registration/form.html',
-                               form=form)
+        if g.meeting.online_registration:
+            Form = custom_form_factory(registration_fields=True,
+                                       form=RegistrationForm)
+            form = Form(request.form)
+            if form.validate():
+                form.save()
+            return render_template('meetings/registration/form.html',
+                                   form=form)
+        return render_template('meetings/registration/closed.html')
---
+++ testsuite/test_meeting_registration.py
@@ -0,0 +1,33 @@
+from flask import url_for
+from pyquery import PyQuery
+
+from .factories import MeetingCategoryFactory
+
+from testsuite.utils import add_participant_custom_fields
+
+
+def test_meeting_online_resistration_open(app):
+    category = MeetingCategoryFactory(meeting__online_registration=True)
+
+    client = app.test_client()
+    with app.test_request_context():
+        add_participant_custom_fields(category.meeting)
+        resp = client.get(url_for('meetings.registration',
+                                  meeting_id=category.meeting.id))
+        form = PyQuery(resp.data)('form')
+        assert form
+
+
+def test_meeting_online_registration_closed(app):
+    category = MeetingCategoryFactory(meeting__online_registration=False)
+
+    client = app.test_client()
+    with app.test_request_context():
+        add_participant_custom_fields(category.meeting)
+        resp = client.get(url_for('meetings.registration',
+                                  meeting_id=category.meeting.id))
+        html = PyQuery(resp.data)
+        form = html('form')
+        alert = html('.alert')
+        assert not form
+        assert alert
