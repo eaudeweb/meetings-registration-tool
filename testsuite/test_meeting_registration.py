27da45b6145eb8fde63e463c4ca0f99ef5fa7999
Taygun <taygun.agiali@eaudeweb.ro>
2014-11-05 12:13:14 +0200

    [Test] Test for user creation after registration

--- testsuite/test_meeting_registration.py
+++ testsuite/test_meeting_registration.py
@@ -4,7 +4,7 @@ from StringIO import StringIO
 from py.path import local
 
 from mrt.mail import mail
-from mrt.models import Participant, ActivityLog
+from mrt.models import Participant, ActivityLog, User
 from .factories import MeetingCategoryFactory, ParticipantFactory
 from .factories import StaffFactory, RoleUserMeetingFactory
 from .factories import UserNotificationFactory, CustomFieldFactory
@@ -83,3 +83,31 @@ def test_meeting_online_registration_with_meeting_photo(app):
         participant = Participant.query.filter_by(meeting=meeting).first()
         assert participant.photo is not None
         assert upload_dir.join(participant.photo).check()
+
+
+def test_meeting_online_registration_and_user_creation(app):
+    category = MeetingCategoryFactory(meeting__online_registration=True)
+    meeting = category.meeting
+
+    data = ParticipantFactory.attributes()
+    data['category_id'] = category.id
+
+    client = app.test_client()
+    with app.test_request_context():
+        add_participant_custom_fields(meeting)
+        populate_participant_form(meeting, data)
+        resp = client.post(url_for('meetings.registration',
+                           meeting_id=meeting.id), data=data)
+        assert resp.status_code == 200
+        assert Participant.query.filter_by(meeting=meeting).count() == 1
+
+        participant = Participant.query.filter_by(meeting=meeting).first()
+        data['email'] = participant.email
+        data['password'] = 'testpassword'
+        data['confirm'] = 'testpassword'
+
+        resp = client.post(url_for('meetings.registration_user',
+                           meeting_id=meeting.id), data=data)
+        assert resp.status_code == 200
+        assert User.query.count() == 1
+        assert participant.user is User.query.get(1)
