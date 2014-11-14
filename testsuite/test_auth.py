7a963615fc41813586749b9c16bc8fd6c3d3780a
Taygun <taygun.agiali@eaudeweb.ro>
2014-11-14 15:46:52 +0200

    Change password on logged user is now reset password

--- testsuite/test_auth.py
+++ testsuite/test_auth.py
@@ -63,27 +63,38 @@ def test_recover_password_fail_after_using_token(app):
 
 
 def test_change_password_succesfully(app):
-    user, data = UserFactory(), UserFactory.attributes()
-    data['new_password'] = data['confirm'] = passwd = str(uuid4())
+    user = UserFactory()
+    data = {'email': user.email}
 
     client = app.test_client()
-    with app.test_request_context():
-        client.post(url_for('auth.login'), data=data)
+    with app.test_request_context(), mail.record_messages() as outbox:
+        with client.session_transaction() as sess:
+            sess['user_id'] = user.id
         resp = client.post(url_for('auth.change_password'), data=data)
         assert resp.status_code == 302
+        assert len(outbox) == 1
 
-    assert user.check_password(passwd)
+        data['password'] = data['confirm'] = passwd = str(uuid4())
+        client.post(url_for('auth.reset', token=user.recover_token), data=data)
+        assert resp.status_code == 302
+        assert user.check_password(passwd)
 
 
 def test_change_password_fail(app):
-    UserFactory()
-    data = UserFactory.attributes()
-    data['new_password'] = data['confirm'] = 'webdeeau'
+    user = UserFactory()
+    data = {'email': user.email}
 
     client = app.test_client()
-    with app.test_request_context():
-        client.post(url_for('auth.login'), data=data)
-        data['password'] = 'baddpass'
+    with app.test_request_context(), mail.record_messages() as outbox:
+        with client.session_transaction() as sess:
+            sess['user_id'] = user.id
         resp = client.post(url_for('auth.change_password'), data=data)
+        assert resp.status_code == 302
+        assert len(outbox) == 1
+
+        data['password'] = str(uuid4())
+        data['confirm'] = 'badpass'
+        resp = client.post(url_for('auth.reset', token=user.recover_token),
+                           data=data)
         assert resp.status_code == 200
         assert len(PyQuery(resp.data)('.alert-danger')) == 1
