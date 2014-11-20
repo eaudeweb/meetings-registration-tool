8cabad4f5a4e51efa8f782c0a15cc4c52b5226b0
Taygun <taygun.agiali@eaudeweb.ro>
2014-11-20 12:31:29 +0200

    [Meeting] Bulk emails are saved as MailLogs

--- testsuite/test_meeting_send_email.py
+++ testsuite/test_meeting_send_email.py
@@ -1,4 +1,5 @@
 from flask import url_for
+from pyquery import PyQuery
 
 from mrt.mail import mail
 from mrt.models import MailLog
@@ -53,6 +54,35 @@ def test_send_email_to_categories(app):
                                    meeting_id=cat_press.meeting.id), data=data)
         assert resp.status_code == 302
         assert len(outbox) == 7
+
+
+def test_send_bulk_email_logs(app):
+    cat = MeetingCategoryFactory()
+    role_user = RoleUserFactory(role__permissions=PERMISSION)
+    ParticipantFactory.create_batch(5, meeting=cat.meeting)
+    ParticipantFactory.create_batch(3, meeting=cat.meeting,
+                                    language='fr')
+
+    data = {
+        'message': 'Test',
+        'subject': 'Test subject',
+        'language': 'English'
+    }
+
+    client = app.test_client()
+    with app.test_request_context(), mail.record_messages() as outbox:
+        with client.session_transaction() as sess:
+            sess['user_id'] = role_user.user.id
+        resp = client.post(url_for('meetings.bulkemail',
+                                   meeting_id=cat.meeting.id), data=data)
+        assert resp.status_code == 302
+        assert len(outbox) == 5
+        assert MailLog.query.filter_by(meeting=cat.meeting).count() == 5
+
+        resp = client.get(url_for('meetings.mail_logs',
+                                  meeting_id=cat.meeting.id))
+        assert resp.status_code == 200
+        assert len(PyQuery(resp.data)('#mails tbody tr')) == 5
 
 
 def test_resend_email(app):
