087fd96bc5fc79d8f7cb13594f95c316ab47d345
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2015-03-03 16:04:21 +0200

    [admin] Refactor global custom fields.

---
+++ mrt/common/custom_fields.py
@@ -0,0 +1,63 @@
+from flask import flash, g
+from flask import render_template, jsonify
+from flask import request, redirect, url_for
+from flask.views import MethodView
+
+from mrt.models import db, CustomField, CustomFieldValue
+
+
+class BaseCustomFieldEdit(MethodView):
+
+    def _get_object(self, custom_field_id=None):
+        return (CustomField.query
+                .filter_by(meeting_id=self.meeting_id, id=custom_field_id)
+                .filter_by(is_primary=False)
+                .first_or_404()
+                if custom_field_id else None)
+
+    def get(self, custom_field_id=None, custom_field_type=None):
+        custom_field = self._get_object(custom_field_id)
+        custom_field_type = custom_field_type or custom_field.custom_field_type
+        form = self.form_class(obj=custom_field,
+                               custom_field_type=custom_field_type)
+        return render_template(self.template, form=form,
+                               custom_field=custom_field)
+
+    def post(self, custom_field_id=None, custom_field_type=None):
+        custom_field = self._get_object(custom_field_id)
+        custom_field_type = custom_field_type or custom_field.custom_field_type
+        form = self.form_class(request.form, obj=custom_field,
+                               custom_field_type=custom_field_type)
+        if form.validate():
+            form.save()
+            flash('Custom field information saved', 'success')
+            return redirect(url_for('.custom_fields'))
+        return render_template(self.template, form=form,
+                               custom_field=custom_field)
+
+    def delete(self, custom_field_id):
+        custom_field = self._get_object(custom_field_id)
+        count = CustomFieldValue.query.filter_by(
+            custom_field=custom_field).count()
+        if count:
+            msg = ("Unable to remove the custom field. There are participants "
+                   "with values for this field.")
+            return jsonify(status="error", message=msg)
+
+        db.session.delete(custom_field)
+        db.session.commit()
+        flash('Custom field successfully deleted', 'warning')
+        return jsonify(status="success", url=url_for('.custom_fields'))
+
+
+class BaseCustomFieldUpdatePosition(MethodView):
+
+    def post(self):
+        items = request.form.getlist('items[]')
+        for i, item in enumerate(items):
+            custom_field = (CustomField.query
+                            .filter_by(id=item, meeting_id=self.meeting_id)
+                            .first_or_404())
+            custom_field.sort = i
+        db.session.commit()
+        return jsonify()
--- mrt/meetings/custom_field.py
+++ mrt/meetings/custom_field.py
@@ -1,6 +1,6 @@
 from flask import current_app as app
 from flask import g
-from flask import render_template, flash, make_response, jsonify
+from flask import render_template, make_response, jsonify
 from flask import request, redirect, url_for
 from flask.views import MethodView
 from sqlalchemy import not_
@@ -15,6 +15,8 @@ from mrt.models import Participant, CustomField, CustomFieldValue, Translation
 
 from mrt.utils import crop_file, unlink_participant_photo
 from mrt.utils import unlink_uploaded_file, rotate_file, unlink_thumbnail_file
+from mrt.common.custom_fields import (
+    BaseCustomFieldEdit, BaseCustomFieldUpdatePosition as BaseUpdatePosition)
 
 
 class CustomFields(PermissionRequiredMixin, MethodView):
@@ -43,52 +45,15 @@ class CustomFields(PermissionRequiredMixin, MethodView):
             custom_fields_for_media=custom_fields_for_media)
 
 
-class CustomFieldEdit(PermissionRequiredMixin, MethodView):
+class CustomFieldEdit(PermissionRequiredMixin, BaseCustomFieldEdit):
 
     permission_required = ('manage_meeting',)
+    form_class = CustomFieldEditForm
+    template = 'meetings/custom_field/edit.html'
 
-    def _get_object(self, custom_field_id=None):
-        return (CustomField.query
-                .filter_by(meeting_id=g.meeting.id, id=custom_field_id)
-                .filter_by(is_primary=False)
-                .first_or_404()
-                if custom_field_id else None)
-
-    def get(self, custom_field_id=None, custom_field_type=None):
-        custom_field = self._get_object(custom_field_id)
-        custom_field_type = custom_field_type or custom_field.custom_field_type
-        form = CustomFieldEditForm(obj=custom_field,
-                                   custom_field_type=custom_field_type)
-        return render_template('meetings/custom_field/edit.html',
-                               form=form,
-                               custom_field=custom_field)
-
-    def post(self, custom_field_id=None, custom_field_type=None):
-        custom_field = self._get_object(custom_field_id)
-        custom_field_type = custom_field_type or custom_field.custom_field_type
-        form = CustomFieldEditForm(request.form, obj=custom_field,
-                                   custom_field_type=custom_field_type)
-        if form.validate():
-            form.save()
-            flash('Custom field information saved', 'success')
-            return redirect(url_for('.custom_fields'))
-        return render_template('meetings/custom_field/edit.html',
-                               form=form,
-                               custom_field=custom_field)
-
-    def delete(self, custom_field_id):
-        custom_field = self._get_object(custom_field_id)
-        count = CustomFieldValue.query.filter_by(
-            custom_field=custom_field).count()
-        if count:
-            msg = ("Unable to remove the custom field. There are participants "
-                   "with values for this field.")
-            return jsonify(status="error", message=msg)
-
-        db.session.delete(custom_field)
-        db.session.commit()
-        flash('Custom field successfully deleted', 'warning')
-        return jsonify(status="success", url=url_for('.custom_fields'))
+    def __init__(self, *args, **kwargs):
+        self.meeting_id = g.meeting.id
+        return super(CustomFieldEdit, self).__init__(*args, **kwargs)
 
 
 class BaseCustomFieldFile(PermissionRequiredMixin, MethodView):
@@ -210,16 +175,10 @@ class CustomFieldCropUpload(BaseCustomFieldFile):
         return redirect(url)
 
 
-class CustomFieldUpdatePosition(PermissionRequiredMixin, MethodView):
+class CustomFieldUpdatePosition(PermissionRequiredMixin, BaseUpdatePosition):
 
     permission_required = ('manage_meeting', )
 
-    def post(self):
-        items = request.form.getlist('items[]')
-        for i, item in enumerate(items):
-            custom_field = (
-                CustomField.query.filter_by(id=item, meeting_id=g.meeting.id)
-                .first_or_404())
-            custom_field.sort = i
-        db.session.commit()
-        return jsonify()
+    def __init__(self, *args, **kwargs):
+        self.meeting_id = g.meeting.id
+        return super(CustomFieldUpdatePosition, self).__init__(*args, **kwargs)
