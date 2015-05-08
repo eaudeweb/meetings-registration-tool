82a4daf74fe4f9dc2d2195ad248f638d104f4882
Dragos Catarahia <dragos@catarahia.me>
2015-05-08 17:04:54 +0300

    Fix upload custom fields bugs

--- mrt/meetings/custom_field.py
+++ mrt/meetings/custom_field.py
@@ -12,7 +12,7 @@ from mrt.forms.meetings import ParticipantEditForm, MediaParticipantEditForm
 from mrt.meetings.mixins import PermissionRequiredMixin
 
 from mrt.models import db
-from mrt.models import Participant, CustomField, CustomFieldValue, Rule
+from mrt.models import Participant, CustomField, Rule
 
 from mrt.utils import crop_file, unlink_participant_custom_file
 from mrt.utils import unlink_uploaded_file, rotate_file, unlink_thumbnail_file
@@ -79,12 +79,30 @@ class BaseCustomFieldFile(PermissionRequiredMixin, MethodView):
                 .filter_by(id=participant_id)
                 .first_or_404())
 
+    def get_custom_field(self, participant, field_slug, **kwargs):
+        if participant.participant_type == Participant.PARTICIPANT:
+            custom_field_type = CustomField.PARTICIPANT
+        else:
+            custom_field_type = CustomField.MEDIA
+        cf = (
+            CustomField.query
+            .filter_by(slug=field_slug, meeting=g.meeting)
+            .filter_by(custom_field_type=custom_field_type))
+        if kwargs:
+            cf = cf.filter_by(**kwargs)
+        return cf.first_or_404()
+
+    def get_custom_field_value(self, cf, participant):
+        return (cf.custom_field_values
+               .filter_by(participant=participant)
+               .first_or_404())
+
 
 class CustomFieldUpload(BaseCustomFieldFile):
 
     def post(self, participant_id, field_slug):
         participant = self.get_object(participant_id)
-        cf = CustomField.query.filter_by(slug=field_slug).first_or_404()
+        cf = self.get_custom_field(participant, field_slug)
 
         if participant.participant_type == Participant.PARTICIPANT:
             form_class = ParticipantEditForm
@@ -98,9 +116,12 @@ class CustomFieldUpload(BaseCustomFieldFile):
 
         if form.validate():
             form.save(participant)
-            filename = cf.custom_field_values.first().value
-            data = get_custom_file_as_filestorage(filename)
-
+            cfv = (cf.custom_field_values.filter_by(participant=participant)
+                   .scalar())
+            if cfv:
+                data = get_custom_file_as_filestorage(cfv.value)
+            else:
+                data = {}
         else:
             return make_response(jsonify(form.errors), 400)
 
@@ -110,14 +131,11 @@ class CustomFieldUpload(BaseCustomFieldFile):
 
     def delete(self, participant_id, field_slug):
         participant = self.get_object(participant_id)
-        custom_field = (
-            CustomFieldValue.query
-            .filter(CustomFieldValue.participant == participant)
-            .filter(CustomFieldValue.custom_field.has(slug=field_slug))
-            .first_or_404()
-        )
-        filename = custom_field.value
-        db.session.delete(custom_field)
+        cf = self.get_custom_field(participant, field_slug)
+        cfv = self.get_custom_field_value(cf, participant)
+
+        filename = cfv.value
+        db.session.delete(cfv)
         db.session.commit()
         unlink_participant_custom_file(filename)
         return jsonify()
@@ -127,18 +145,16 @@ class CustomFieldRotate(BaseCustomFieldFile):
 
     def post(self, participant_id, field_slug):
         participant = self.get_object(participant_id)
-        custom_field = CustomField.query.filter_by(
-            slug=field_slug, field_type='image').first_or_404()
-        custom_field_value = CustomFieldValue.query.filter_by(
-            participant=participant, custom_field=custom_field
-        ).first_or_404()
+        cf = self.get_custom_field(participant, field_slug,
+                                   field_type='image')
+        cfv = self.get_custom_field_value(cf, participant)
 
-        newfile = rotate_file(custom_field_value.value, 'custom')
-        if newfile == custom_field_value.value:
+        newfile = rotate_file(cfv.value, 'custom')
+        if newfile == cfv.value:
             return make_response(jsonify(), 400)
 
-        unlink_participant_custom_file(custom_field_value.value)
-        custom_field_value.value = newfile
+        unlink_participant_custom_file(cfv.value)
+        cfv.value = newfile
         db.session.commit()
 
         data = get_custom_file_as_filestorage(newfile)
@@ -151,22 +167,18 @@ class CustomFieldCropUpload(BaseCustomFieldFile):
 
     def get(self, participant_id, field_slug):
         participant = self.get_object(participant_id)
-        custom_field = CustomField.query.filter_by(
-            slug=field_slug, field_type='image').first_or_404()
-        custom_field_value = CustomFieldValue.query.filter_by(
-            participant=participant, custom_field=custom_field
-        ).first_or_404()
+        cf = self.get_custom_field(participant, field_slug,
+                                   field_type='image')
+        cfv = self.get_custom_field_value(cf, participant)
         return render_template('meetings/custom_field/crop.html',
                                participant=participant,
-                               data=custom_field_value.value)
+                               data=cfv.value)
 
     def post(self, participant_id, field_slug):
         participant = self.get_object(participant_id)
-        custom_field = CustomField.query.filter_by(
-            slug=field_slug, field_type='image').first_or_404()
-        custom_field_value = CustomFieldValue.query.filter_by(
-            participant=participant, custom_field=custom_field
-        ).first_or_404()
+        cf = self.get_custom_field(participant, field_slug,
+                                   field_type='image')
+        cfv = self.get_custom_field_value(cf, participant)
 
         form = request.form
         x1 = int(form.get('x1', 0, type=float))
@@ -174,13 +186,13 @@ class CustomFieldCropUpload(BaseCustomFieldFile):
         x2 = int(form.get('x2', 0, type=float))
         y2 = int(form.get('y2', 0, type=float))
 
-        unlink_uploaded_file(custom_field_value.value, 'crop',
+        unlink_uploaded_file(cfv.value, 'crop',
                              dir_name=app.config['PATH_CUSTOM_KEY'])
-        unlink_thumbnail_file(custom_field_value.value, dir_name='crops')
+        unlink_thumbnail_file(cfv.value, dir_name='crops')
 
         valid_crop = x2 > 0 and y2 > 0
         if valid_crop:
-            crop_file(custom_field_value.value, 'custom', (x1, y1, x2, y2))
+            crop_file(cfv.value, 'custom', (x1, y1, x2, y2))
         if participant.participant_type == Participant.PARTICIPANT:
             url = url_for('.participant_detail', participant_id=participant_id)
         else:
