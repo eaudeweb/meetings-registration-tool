03623545b4ff0f129a2600311b3a38c0f1c41f41
Razvan Chitu <razvan.chitu@eaudeweb.ro>
2015-08-24 12:11:32 +0300

    Added command for switching to the official UN countries list

--- manage.py
+++ manage.py
@@ -4,12 +4,13 @@ from click import CommandCollection
 from mrt.app import create_app
 from mrt.manager import cli as manager_cli
 from contrib.importer import cli as importer_cli
+from contrib.countries_un import cli as countries_cli
 
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    cli = CommandCollection(sources=[manager_cli, importer_cli])
+    cli = CommandCollection(sources=[manager_cli, importer_cli, countries_cli])
     cli(obj={'app': app})
 
