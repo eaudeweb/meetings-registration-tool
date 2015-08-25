53f2629e9c58e3c1dfa0c0bcf39bfb6cec45c48f
Razvan Chitu <razvan.chitu@eaudeweb.ro>
2015-08-25 11:59:42 +0300

    Refs #3162 - Updated cloning and duplicating categories to include the tags attribute

--- mrt/forms/meetings/meeting.py
+++ mrt/forms/meetings/meeting.py
@@ -123,6 +123,7 @@ class MeetingEditForm(BaseForm):
             category = copy_attributes(Category(), category_default)
             category.title = copy_attributes(Translation(),
                                              category_default.title)
+            category.tags = category_default.tags
             category.meeting = meeting
             db.session.add(category)
             db.session.flush()
@@ -214,6 +215,15 @@ class MeetingCloneForm(MeetingEditForm):
 
             db.session.flush()
 
+    def _clone_categories(self, meeting, categories):
+        for category in categories:
+            clone = copy_attributes(Category(), category)
+            clone.tags = category.tags
+            clone.title = copy_attributes(Translation(), category.title)
+            clone.meeting = meeting
+            db.session.add(clone)
+            db.session.flush()
+
     def _clone_rules(self, meeting, rules):
         for rule in rules:
             rule_clone = copy_attributes(rule.__class__(), rule,
@@ -260,7 +270,7 @@ class MeetingCloneForm(MeetingEditForm):
         meeting.photo_field_id = meeting.photo_field_id or None
         meeting.media_photo_field_id = meeting.media_photo_field_id or None
         self._clone_custom_fields(meeting, self.obj.custom_fields, ('label', ))
-        self._clone_relation(meeting, self.obj.categories, ('title', ))
+        self._clone_categories(meeting, self.obj.categories)
         self._clone_relation(meeting, self.obj.phrases, ('description', ))
         self._clone_relation(meeting, self.obj.role_users, exclude_fk=False)
         self._clone_relation(meeting, self.obj.user_notifications,
