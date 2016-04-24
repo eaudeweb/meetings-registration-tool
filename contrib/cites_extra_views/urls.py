6366a6a5d7e15336c3d868abc0cd561cb641c9bb
Dragos Catarahia <dragos@catarahia.me>
2016-04-24 22:38:27 +0200

    Add extra items in printout menu

--- contrib/cites_extra_views/urls.py
+++ contrib/cites_extra_views/urls.py
@@ -31,3 +31,24 @@ blueprint.add_url_rule(
 
 blueprint.url_defaults(add_meeting_id)
 blueprint.url_value_preprocessor(add_meeting_global)
+
+
+EXTRA_PRINTOUTS_MENU = (
+    ('Print list of media participants',
+     'cites_extra_views.printouts_media'),
+    ('Print list of credentials',
+     'cites_extra_views.printouts_credentials'),
+    ('Print pigeon holes',
+     'meetings.printouts_pigeon_holes'),
+    ('Print list of participants for verification',
+     'cites_extra_views.printouts_verification'),
+    ('Print list of announced parties',
+     'cites_extra_views.printouts_parties'),
+    ('Print list of announced observers',
+     'cites_extra_views.printouts_observers'),
+)
+
+
+@blueprint.app_context_processor
+def inject_context():
+    return {'EXTRA_PRINTOUTS_MENU': EXTRA_PRINTOUTS_MENU}
